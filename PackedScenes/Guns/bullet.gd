369161611a1616809e02c3eaeb7ff4e78c1fd91c
Bryan <bryanguzman859@gmail.com>
2026-07-06 17:28:59 -0400

    - Improved bullet.gd

--- PackedScenes/Guns/bullet.gd
+++ PackedScenes/Guns/bullet.gd
@@ -1,14 +1,14 @@
 class_name Bullet
 extends Node2D
 
-
 @export var bullet_speed : float
 @onready var hit_box: Area2D = $HitBox
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	hit_box.body_entered.connect(func(body: Node2D):
+		queue_free()
+		)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
