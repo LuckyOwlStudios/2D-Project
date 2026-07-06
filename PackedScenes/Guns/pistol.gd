e991e21ccf52cb96786963e303efaf40e2407585
Bryan <bryanguzman859@gmail.com>
2026-07-06 17:49:53 -0400

    - Fixed up the pistol.gd 	- Removed unnecessary methods 	- Improved accuracy of bullet to mouse

--- PackedScenes/Guns/pistol.gd
+++ PackedScenes/Guns/pistol.gd
@@ -6,21 +6,11 @@ extends Node2D
 @onready var bullet_spawn_point: Marker2D = $BulletSpawnPoint
 @onready var node_path_to_world := get_node("/root/World")
 const BULLET = preload("uid://cyftgfviauafs")
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot"):
 		animated_sprite_2d.play("shoot")
-		var new_bullet := BULLET.instantiate()
+		var new_bullet := BULLET.instantiate() as Node2D
 		new_bullet.position = bullet_spawn_point.global_position
-		new_bullet.rotation = bullet_spawn_point.global_rotation
+		new_bullet.look_at(get_global_mouse_position())
 		node_path_to_world.add_child(new_bullet)
