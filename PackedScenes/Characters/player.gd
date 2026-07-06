e029e438572e0d6d0f2d4aa95620e420c53fbd59
Bryan <bryanguzman859@gmail.com>
2026-07-06 12:59:04 -0400

    - Updated Char

--- PackedScenes/Characters/player.gd
+++ PackedScenes/Characters/player.gd
@@ -18,14 +18,18 @@ enum POS_STATE {
 ## How quickly the player reaches top speed, in pixels per second squared.
 @export var acceleration: float = 2000.0
 
+@export var look_nodes: Array[Node2D]
+
+@onready var camera: Camera2D = $Camera2D
 @onready var sprite_root: Node2D = $SpriteRoot
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 @onready var center: Marker2D = $Center
-@onready var ray_cast: RayCast2D = %RayCast2D
 
 ## Grace period (seconds) after leaving a ledge where a jump still counts.
 var coyote_time: float = 0.1
 var _coyote_timer: float = 0.0
+
+var is_left: bool
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -33,9 +37,9 @@ func _input(event: InputEvent) -> void:
 
 func _process(delta: float) -> void:
 	var mouse_location: Vector2 = get_global_mouse_position()
-	var is_left: bool = mouse_location.x <= global_position.x
+	is_left = mouse_location.x <= global_position.x
 	sprite_root.scale.x = -1 if is_left else 1
-	_handle_head()
+	_handle_look_nodes()
 
 func _physics_process(delta: float) -> void:
 	
@@ -55,5 +59,7 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 
-func _handle_head():
-	pass
+func _handle_look_nodes() -> void:
+	var mouse: Vector2 = get_global_mouse_position()
+	for node in look_nodes:
+		node.look_at(mouse)
